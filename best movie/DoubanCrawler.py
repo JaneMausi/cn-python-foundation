34e2e5a0fff368a5550ce5f6e311d620d5a475fa
Lixuan Jia <janejia1019@gmail.com>
2018-08-16 13:13:01 +0200

    modify analyzing errors

--- best movie/DoubanCrawler.py
+++ best movie/DoubanCrawler.py
@@ -11,13 +11,13 @@ location_list = ["中国大陆", "美国", "香港", "台湾", "日本", "韩国
 
 
 class Movie:
-    def __init__(self, name, rate, location, category, info_link, cover_link):
-        self.name = name
-        self.rate = rate
-        self.location = location
-        self.category = category
-        self.info_link = info_link
-        self.cover_link = cover_link
+    def __init__(self, info):
+        self.name = info[0]
+        self.rate = info[1]
+        self.location = info[2]
+        self.category = info[3]
+        self.info_link = info[4]
+        self.cover_link = info[5]
 
     def get_movie_info(self):
         return [self.name, self.rate, self.location, self.category,
@@ -31,8 +31,8 @@ def getMovieUrl(category, location):
     url_attr = '&'.join(["sort=S", "range=9,10", "tags={}".format(url_tags)])
     movie_url = url_body + "?" + url_attr
     return movie_url
-	
-	
+
+
 # input is html text, output is dictionary of a movie's information
 def getMovies(category, location):
     movies = []
@@ -43,16 +43,16 @@ def getMovies(category, location):
     for item in alist:
         cover = item.find(class_='cover-wp').find(class_='pic').img
         information = item.p
-        m = Movie(information.find(class_='title').text, information.find(class_='rate').text,
-                  location, category, item.get('href'), cover.get('src'))
+        m = Movie([information.find(class_='title').text, information.find(class_='rate').text,
+                  location, category, item.get('href'), cover.get('src')])
         movies.append(m)
     return movies
 
 
 if __name__ == "__main__":
-    # get movies
+    # get movie list
     movie_dict = {}
-    for item_category in ["剧情", "喜剧", "动作"]:
+    for item_category in ["剧情", "喜剧", "奇幻"]:
         for item_location in location_list:
             if item_category not in movie_dict:
                 movie_dict[item_category] = getMovies(item_category, item_location)
@@ -64,37 +64,45 @@ if __name__ == "__main__":
         for key in movie_dict:
             for movie in movie_dict[key]:
                 writer.writerow(movie.get_movie_info())
+    # debug
+    '''
+    movie_dict = {}
+    with open('movies.csv', 'r', encoding='utf-8') as csvfile:
+        for line in csvfile:
+            info = line.strip('\n').split('&')
+            m = Movie(info)
+            if info[3] not in movie_dict:
+                movie_dict[info[3]] = []
+                movie_dict[info[3]].append(m)
+            else:
+                movie_dict[info[3]].append(m)
+    '''
     # analyze
-    statistics = {}
-    statistics_reverse = {}
     output = ''
     for category in movie_dict:
+        statistics = {}
         movie_list = movie_dict[category]
         statistics['all'] = len(movie_list)
         for movie in movie_list:
             if movie.location not in statistics:
-                statistics[movie.location] = 0
+                statistics[movie.location] = 1
             else:
                 statistics[movie.location] += 1
-
-        statistics_reverse = {value:key for key, value in statistics.items()}
+        # when there is no movie in this category
+        if 0 == len(statistics):
+            output = output + "\n\n类别: {}\n该类别无电影资源\n".format(category)
+            continue
+        #
+        re_stat = {value:key for key, value in statistics.items()}
         rank = sorted(statistics.values(), reverse=True)
-        if len(rank) < 4:
-            exit(1)
-
-        output = output + "类别: {}\n" \
-                          "排名前三地区: {}, {}, {}\n" \
-                          "分别占比: {} {}, {} {}, {} {}\n\n\n".format(category,
-                                                                   statistics_reverse[rank[1]],
-                                                                   statistics_reverse[rank[2]],
-                                                                   statistics_reverse[rank[3]],
-                                                                   statistics_reverse[rank[1]],
-                                                                   format(float(rank[1])/float(rank[0]) * 100, '.2f'),
-                                                                   statistics_reverse[rank[2]],
-                                                                   format(float(rank[2])/float(rank[0]) * 100, '.2f'),
-                                                                   statistics_reverse[rank[3]],
-                                                                   format(float(rank[3])/float(rank[0]) * 100, '.2f'))
-
+        output = output + "\n\n类别: {}\n电影数量排名前列地区: \n".format(category)
+        for i in range(len(rank)):
+            if 0 == i:
+                continue
+            if i > 3:
+                break
+            output = output + \
+                     "  {} {}\n".format(re_stat[rank[i]], format(float(rank[i])/float(rank[0]) * 100, '.2f'))
         # create output.txt
         with open('output.txt', 'w') as f:
             f.write(output)
